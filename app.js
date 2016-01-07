abc35d3f057e0d7fedc8f3c28a01d0db175a81f1
Sam Gruse <sgruse89@gmail.com>
2016-01-07 12:46:13 -0800

    I changed my code so that it looks a lot neater for lab 3

--- app.js
+++ app.js
@@ -5,60 +5,37 @@ var user = prompt("Welcome to my guessing game, what is your name?");
 alert("Hello " + user + ", I'm going to ask you a few questions now and see how well you know me");
 console.log("The user's name is " + user);
 
-
 var answer1 = prompt("Please answer Y or N, do you think baseball is my favorite sport?").toUpperCase();
-if (answer1 === "Y" || answer1 === "YES"){
+  if (answer1 === "Y" || answer1 === "YES"){
   console.log(user + " replied " + answer1)
   alert("I'm sorry " + user + " but that's not correct, football is my favorite sport");
 }
-else if (answer1 === "N" || answer1 === "NO") {
+  else if (answer1 === "N" || answer1 === "NO") {
   console.log(user + " replied " + answer1)
   alert("You're right! My favorite sport is football. Great answer!");
   counter += 1;
-}
-else {
-  alert("That's not an acceptable answer please try again");
-  answer1 = prompt("Please answer Y or N, do you think baseball is my favorite sport?").toUpperCase();
-  if (answer1 === "Y" || answer1 === "YES"){
-    console.log(user + " replied " + answer1)
-    alert("I'm sorry " + user + " but that's not correct, football is my favorite sport");
-  }
-  else if (answer1 === "N" || answer1 === "NO") {
-    alert("You're right! My favorite sport is football. Great answer!");
-    counter += 1;
   }
   else {
-    alert("That's not an acceptable answer, lets move on");}
-}
+  alert("That's not an acceptable answer lets move on to the next question");
+  }
 
 var answer2 = prompt("Next question, am I allergic to almonds? Please answer with either Y or N").toUpperCase();
-if (answer2 === "Y" || answer2 === "YES"){
-  console.log(user + " replied " + answer2);
-  alert("WRONG! I love almonds, they're a great source of fat and nutrients");
-}
-  else if (answer2 === "N" || answer2 === "NO")
-{
-  console.log(user + " replied " + answer2)
-  alert("You're correct! I'm not allergic to almonds and eat them all the time");
-  counter += 1;
-}
-  else {
-    alert("That's not an acceptable answer, please try again");
-    answer2 = prompt("Next question, am I allergic to almonds? Please answer with either Y or N").toUpperCase();
-    if (answer2 === "Y" || answer2 === "YES"){
-      alert("WRONG! I love almonds, they're a great source of fat and nutrients");
+  if (answer2 === "Y" || answer2 === "YES"){
+    console.log(user + " replied " + answer2);
+    alert("WRONG! I love almonds, they're a great source of fat and nutrients");
     }
-      else if (answer2 === "N" || answer2 === "NO")
+    else if (answer2 === "N" || answer2 === "NO")
     {
-      alert("You're correct! I'm not allergic to almonds and eat them all the time");
+    console.log(user + " replied " + answer2)
+    alert("You're correct! I'm not allergic to almonds and eat them all the time");
+    counter += 1;
     }
-      else {
-        alert("That's not an acceptable answer, lets move on");
+    else {
+      alert("That's not an acceptable answer, lets move on to the next question");
       }
-    }
 
-    alert("Okay " + user + " this is the final question");
-    var foodAnswer = prompt("Which do you think is my favorite food? Pizza, chicken strips or french fries?").toUpperCase();
+  alert("Okay " + user + " this is the final question");
+  var foodAnswer = prompt("Which do you think is my favorite food? Pizza, chicken strips or french fries?").toUpperCase();
     var answer3 = false;
     while (answer3 === false) {
     switch(foodAnswer){
@@ -87,14 +64,14 @@ if (answer2 === "Y" || answer2 === "YES"){
 
   var answer4 = prompt("Wait!!! One last BONUS question. How old do you think I am?");
   answer4 = parseInt(answer4);
-  while (answer4 !== 26){
+    while (answer4 !== 26){
     if (answer4 > 26){
     alert("Wrong, I'm younger than that");
-  }
-  else
-  alert("Wrong, I'm older than that");
+    }
+    else
+    alert("Wrong, I'm older than that");
     answer4 = parseInt(prompt("You're getting closer, try again"));
-}
+  }
 
 counter += 1;
 alert("Wow " + user + " you know me better than I thought you did and got " + counter + " questions correctly. Thanks for playing my guessing game! We can be friends now.");
