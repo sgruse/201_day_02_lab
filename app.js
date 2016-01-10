26bdc54d48d4f85baeaa74cfa4823d0da06f7d0c
Sam Gruse <sgruse89@gmail.com>
2016-01-09 18:28:09 -0800

    I'm not able to get my new user's name into the text field.  Refering back to other project

--- app.js
+++ app.js
@@ -3,41 +3,38 @@ var res1 = document.getElementById('resultOne');
 var res2 = document.getElementById('resultTwo');
 var res3 = document.getElementById('resultThree');
 var res4 = document.getElementById('resultFour');
+var userName = document.getElementById('newUser');
 var counter = 0;
 
 var user = prompt('Welcome to my guessing game, what is your name?');
 alert("Hello " + user + ", I'm going to ask you a few questions now and see how well you know me");
 console.log('The user\'s name is ' + user);
-
+userName.textContent = 'Hello ' + user + '!';
+//Qestion One array data
 var questData = [['Do you think baseball is my favorite sport?', 'YES', 'Y', 'I\'m sorry ' + user + ' but that\'s not correct football is my favorite sport', 'N', 'NO', 'You\'re right! My favorite sport is football. Great answer!', res1],
+//Question Two array data
 ['Next question, am I allergic to almonds? Please answer with either Y or N', 'Y', 'YES', 'WRONG! I love almonds, they\'re a great source of fat and nutrients', 'N', 'NO', 'You\'re correct! I\'m not allergic to almonds and eat them all the time', res2]
 ]
-
-
+//Function for qustions one and two
 function questions() {
   var answer1 = prompt(questData [i][0]).toUpperCase();
   if (answer1 === questData[i][1] || answer1 === questData[i][2]){
     console.log(user + " replied " + answer1);
-    // alert("I'm sorry " + user + " but that's not correct, football is my favorite sport");
   questData[i][7].textContent = questData[i][3]
   questData[i][7].className = "wrong";
-  }
-  else if (answer1 === "N" || answer1 === "NO") {
+  } else if (answer1 === questData[i][4]|| answer1 === questData[i][5]) {
     console.log(user + " replied " + answer1);
-    // alert("You're right! My favorite sport is football. Great answer!");
     questData[i][7].textContent = questData[i][6];
     questData[i][7].className = "correct";
     counter += 1;
-  }
-  else {
+  } else {
     alert("That's not an acceptable answer lets move on to the next question");
   }
 }
 for (var i = 0; i < questData.length; i++) {
   questions();
 }
-
-function thirdQuestion() {
+function thirdQuestion() {   //This is switch statement I made for practice though I realize we won't be using them hardly at all.
   alert("Okay " + user + " this is the final question");
   var foodAnswer = prompt("Which do you think is my favorite food? Pizza, chicken strips or french fries?").toUpperCase();
   var answer3 = false;
@@ -73,11 +70,9 @@ function thirdQuestion() {
       var foodAnswer = prompt("Which do you think is my favorite food? Pizza, chicken strips or french fries?").toUpperCase();
     }
   }
-
 }
 function fourthQuestion() {
 var response4 = false;
-
   while(response4 === false) {
     var answer4 = prompt("Wait!!! One last BONUS question. How old do you think I am?");
     if(parseInt(answer4) < 26) {
@@ -93,11 +88,10 @@ var response4 = false;
     response4 = true;
     }
   }
-  
 }
+//Calling the third and fourth question which couldn't be included in the questions' function
 thirdQuestion();
 fourthQuestion();
-
 
 counter += 1;
 alert("Wow " + user + " you know me better than I thought you did and got " + counter + " questions correctly. Thanks for playing my guessing game! We can be friends now.");
